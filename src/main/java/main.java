dffe1db06266efbba33d1d9f556265615fab07d3
Shady GOMAA <shady.gomaa@idemia.com>
2021-10-03 15:26:12 +0200

    final commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.JavascriptExecutor;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.junit.Assert;
 
 public class main extends Capabilities{
 
@@ -13,22 +14,21 @@ public class main extends Capabilities{
 		// TODO satrt app
 		AndroidDriver<AndroidElement> driver=baseCapabilities();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("https://www.youtube.com/");
+		
+		driver.get("https://rahulshettyacademy.com/angularAppdemo/");
 
-//		driver.findElement(By.cssSelector(".navbar-toggler")).click();
-//
-//		driver.findElement(By.cssSelector("a[href*='products']")).click();
-//
-//		JavascriptExecutor js= (JavascriptExecutor)driver;
-//
-//		js.executeScript("window.scrollBy(0,1000)", "");
-//
-//		String text =driver.findElement(By.xpath("(//li[@class='list-group-item'])[3]/div/div/a")).getText();
+		driver.findElement(By.cssSelector(".navbar-toggler")).click();
 
-//		Assert.assertEquals(text, "Devops");
+		driver.findElement(By.cssSelector("a[href*='products']")).click();
 
+		JavascriptExecutor js= (JavascriptExecutor)driver;
 
-		driver.get("https://www.youtube.com/watch?v=Nyw6P6XMdU8");
+		js.executeScript("window.scrollBy(0,1000)", "");
+
+		String text =driver.findElement(By.xpath("(//li[@class='list-group-item'])[3]/div/div/a")).getText();
+
+		Assert.assertEquals(text, "Devops");
+		
 				
 		
 	}
