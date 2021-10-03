4a8ff59c30cbfc6b0b89e88ba17ff66a811d80e1
Shady GOMAA <shady.gomaa@idemia.com>
2021-10-03 14:23:07 +0200

    first commit 3-10-2021

---
+++ src/main/java/Capabilities.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class Capabilities {
+
+	public static AndroidDriver<AndroidElement> baseCapabilities() throws MalformedURLException {
+		// TODO Auto-generated method stub
+
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+
+		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "shady1");
+
+		capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator1");
+
+		capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "chrome");
+		
+		capabilities.setCapability("chromedriverExecutable","C:\\webdrivers\\chromedriver.exe");
+		
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		
+		return driver;
+	}
+
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,36 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class main extends Capabilities{
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO satrt app
+		AndroidDriver<AndroidElement> driver=baseCapabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//		driver.get("https://www.youtube.com/");
+
+//		driver.findElement(By.cssSelector(".navbar-toggler")).click();
+//
+//		driver.findElement(By.cssSelector("a[href*='products']")).click();
+//
+//		JavascriptExecutor js= (JavascriptExecutor)driver;
+//
+//		js.executeScript("window.scrollBy(0,1000)", "");
+//
+//		String text =driver.findElement(By.xpath("(//li[@class='list-group-item'])[3]/div/div/a")).getText();
+
+//		Assert.assertEquals(text, "Devops");
+
+
+		driver.get("https://www.youtube.com/watch?v=Nyw6P6XMdU8");
+				
+		
+	}
+
+}
